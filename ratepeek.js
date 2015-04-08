bdadcc75a6efebb2b921a9852f0bae667e196c3c
Luke Agius <luke.agius@gmail.com>
2015-04-08 20:27:28 +1200

    Added web-server

--- ratepeek.js
+++ ratepeek.js
@@ -1,29 +1,35 @@
 (function(){ 
 	
+	// Setting JsonRates API Key
 	JR.apikey('jr-92758bdeda026d0f7fcc154b8ea0b86d');
+
+	// Init Angular App
 	var app = angular.module("ratepeek", ["ngRoute"]);
 
-	// ========================== Routing ===============================
+	// Angular Routing
 	app.config(function($routeProvider){
 		$routeProvider
-			.when('/:fromCurrency/:toCurrency',{
+			.when('/:fromCurrency/:toCurrency/:amount',{
 				templateUrl : 'Views/RatePeeker.html',
 				controller 	: "GeneralController"
 			})
 	});
 
-	// ========================== Controller ===============================
+	// Angular Controller
 	app.controller("GeneralController", ["$scope","$routeParams",
 		function($scope,$routeParams){
 			
+			// Converting from first currency to second currency in the url 
 			JR.from($routeParams.fromCurrency).to($routeParams.toCurrency).get(function(result) {
 				document.getElementById("fromToRate").innerText = result.rate;
 			});
 
+			// Converting from second currency to first currency in the url 
 			JR.from($routeParams.toCurrency).to($routeParams.fromCurrency).get(function(result) {
 				document.getElementById("toFromRate").innerText = result.rate;
 			});
 
+			// Passing selected currencies from URL
 			$scope.from = $routeParams.fromCurrency;
 			$scope.to = $routeParams.toCurrency;
 	}]);
